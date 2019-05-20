8a227245e0b87550c75b4484b25bf299e66d5ef0
namratarattan <namratarattan@192.168.0.14>
2019-05-20 15:47:00 -0400

    Km to miles

---
+++ Project1/src/kmToMiles.java
@@ -0,0 +1,26 @@
+
+public class kmToMiles {
+	
+	
+	public static void main() {
+		
+		
+		double toMilesPerHour ;
+		
+		double kilometersPerHour = 0;
+		
+		if (kilometersPerHour <=0)
+			
+		{
+			System.out.println("this is less then zero");
+			}
+		else if (kilometersPerHour > 0)
+			{toMilesPerHour = kilometersPerHour * 1.609;
+		System.out.println("your answer is " + toMilesPerHour);
+		
+	}
+
+	
+
+}
+}
